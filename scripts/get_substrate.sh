5ccbec7fabf3ddc118da3e3567bfaa8bfa3c0ac3
Jared Flatow <jflatow@gmail.com>
2021-01-22 13:53:05 -0800

    parent b4be51827c6fbf170bf56fd574fbc0d5f13b1c64 author Jared Flatow <jflatow@gmail.com> 1611352385 -0800 committer Coburn Berry <coburn@compound.finance> 1613524314 -0800
    WIP: Build/upload the binary on master
    
    try the build on architecture plugin
    
    change to push tags from create
    
    x
    
    test more
    
    eh
    
    plz
    
    plz
    
    plz3
    
    plz4
    
    plz5
    
    plz6
    
    plz7
    
    it ok
    
    mon
    
    monn
    
    monnn
    
    monnnn
    
    monnnnn
    
    monnnnnn
    
    monnnnnnn
    
    change release name

--- scripts/get_substrate.sh
+++ scripts/get_substrate.sh
@@ -3,64 +3,64 @@
 
 if ! which rustup >/dev/null 2>&1; then
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	set -e
-	if [[ $(whoami) == "root" ]]; then
-	    MAKE_ME_ROOT=
-	else
-	    MAKE_ME_ROOT=sudo
-	fi
+		set -e
+		if [[ $(whoami) == "root" ]]; then
+			MAKE_ME_ROOT=
+		else
+			MAKE_ME_ROOT=sudo
+		fi
 
-	if [ -f /etc/redhat-release ]; then
-	    echo "Redhat Linux detected."
-	    echo "This OS is not supported with this script at present. Sorry."
-	    echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	    exit 1
-	elif [ -f /etc/SuSE-release ]; then
-	    echo "Suse Linux detected."
-	    echo "This OS is not supported with this script at present. Sorry."
-	    echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	    exit 1
-	elif [ -f /etc/arch-release ]; then
-	    echo "Arch Linux detected."
-	    $MAKE_ME_ROOT pacman -Syu --needed --noconfirm cmake gcc openssl-1.0 pkgconf git clang
-	    export OPENSSL_LIB_DIR="/usr/lib/openssl-1.0";
-	    export OPENSSL_INCLUDE_DIR="/usr/include/openssl-1.0"
-	elif [ -f /etc/mandrake-release ]; then
-	    echo "Mandrake Linux detected."
-	    echo "This OS is not supported with this script at present. Sorry."
-	    echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	    exit 1
-	elif [ -f /etc/debian_version ]; then
-	    echo "Ubuntu/Debian Linux detected."
-	    $MAKE_ME_ROOT apt update
-	    $MAKE_ME_ROOT apt install -y cmake pkg-config libssl-dev git gcc build-essential git clang libclang-dev
-	else
-	    echo "Unknown Linux distribution."
-	    echo "This OS is not supported with this script at present. Sorry."
-	    echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	    exit 1
-	fi
+		if [ -f /etc/redhat-release ]; then
+			echo "Redhat Linux detected."
+			echo "This OS is not supported with this script at present. Sorry."
+			echo "Please refer to https://github.com/paritytech/substrate for setup information."
+			exit 1
+		elif [ -f /etc/SuSE-release ]; then
+			echo "Suse Linux detected."
+			echo "This OS is not supported with this script at present. Sorry."
+			echo "Please refer to https://github.com/paritytech/substrate for setup information."
+			exit 1
+		elif [ -f /etc/arch-release ]; then
+			echo "Arch Linux detected."
+			$MAKE_ME_ROOT pacman -Syu --needed --noconfirm cmake gcc openssl-1.0 pkgconf git clang
+			export OPENSSL_LIB_DIR="/usr/lib/openssl-1.0";
+			export OPENSSL_INCLUDE_DIR="/usr/include/openssl-1.0"
+		elif [ -f /etc/mandrake-release ]; then
+			echo "Mandrake Linux detected."
+			echo "This OS is not supported with this script at present. Sorry."
+			echo "Please refer to https://github.com/paritytech/substrate for setup information."
+			exit 1
+		elif [ -f /etc/debian_version ]; then
+			echo "Ubuntu/Debian Linux detected."
+			$MAKE_ME_ROOT apt update
+			$MAKE_ME_ROOT apt install -y cmake pkg-config libssl-dev git gcc build-essential git clang libclang-dev curl
+		else
+			echo "Unknown Linux distribution."
+			echo "This OS is not supported with this script at present. Sorry."
+			echo "Please refer to https://github.com/paritytech/substrate for setup information."
+			exit 1
+		fi
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-	set -e
-	echo "Mac OS (Darwin) detected."
+			set -e
+			echo "Mac OS (Darwin) detected."
 
-	if ! which brew >/dev/null 2>&1; then
-	    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		if ! which brew >/dev/null 2>&1; then
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		fi
+
+		brew update
+		brew install openssl cmake llvm
+	elif [[ "$OSTYPE" == "freebsd"* ]]; then
+		echo "FreeBSD detected."
+		echo "This OS is not supported with this script at present. Sorry."
+		echo "Please refer to https://github.com/paritytech/substrate for setup information."
+		exit 1
+	else
+		echo "Unknown operating system."
+		echo "This OS is not supported with this script at present. Sorry."
+		echo "Please refer to https://github.com/paritytech/substrate for setup information."
+		exit 1
 	fi
-
-	brew update
-	brew install openssl cmake llvm
-    elif [[ "$OSTYPE" == "freebsd"* ]]; then
-	echo "FreeBSD detected."
-	echo "This OS is not supported with this script at present. Sorry."
-	echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	exit 1
-    else
-	echo "Unknown operating system."
-	echo "This OS is not supported with this script at present. Sorry."
-	echo "Please refer to https://github.com/paritytech/substrate for setup information."
-	exit 1
-    fi
 
     curl https://sh.rustup.rs -sSf | sh -s -- -y
     source ~/.cargo/env
